1a0c31d6fa9f2484cdf7507f8bf0ba911f5dd157
Daniel Underwood <daniel.underwood13@gmail.com>
2014-10-24 07:49:18 -0400

    Read in file and make basic plot

--- dataanalysis.py
+++ dataanalysis.py
@@ -1,1 +1,26 @@
-__author__ = 'danielunderwood'
+import sys
+import csv
+import matplotlib.pyplot as plt
+
+if(len(sys.argv) != 2):
+    print "Usage: python dataanalysis.py <filename>"
+    quit()
+
+# Set Data filename
+filename = sys.argv[1]
+
+with open(filename, 'rb') as file:
+    # Get data
+    data = csv.reader(file, delimiter=',')
+
+    # Assign data appropriately
+    wavelength = []
+    absorption = []
+    for row in data:
+        wavelength.append(float(row[0]))
+        absorption.append(float(row[1]))
+
+plt.plot(wavelength, absorption)
+plt.show()
+
+
