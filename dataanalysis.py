db8f7bda9398b3a264eb53e47c607499fb2d4f3f
Daniel Underwood <daniel.underwood13@gmail.com>
2014-10-24 09:31:35 -0400

    Add homo/lumo levels for molecules

--- dataanalysis.py
+++ dataanalysis.py
@@ -10,8 +10,7 @@ if len(sys.argv) < 2 or sys.argv[1] == 'help' or sys.argv[1] == 'h' or sys.argv[
 # Define Constants
 h = 4.135667e-15        # Planck's constant in eV*s
 c = 299792458e9         # Speed of Light in nm/s
-me = 5.10998910e-11    # Electron Mass in eV/c^2 where c is in nm
-
+me = 5.10998910e-11     # Electron Mass in eV/c^2 where c is in nm
 
 # Set Data filename and molecule name
 filename = sys.argv[1]
@@ -19,6 +18,20 @@ if len(sys.argv) == 3:
     molecule = sys.argv[2]
 else:
     molecule = filename
+
+
+# Define HOMO Levels Depending on Molecule
+if 'naphthalene' in molecule.lower():
+    HOMO = 5
+elif 'anthracene' in molecule.lower():
+    HOMO = 7
+elif 'tetracene' in molecule.lower():
+    HOMO = 9
+else:
+    print("No defined HOMO level for {0}. Exiting...".format(molecule))
+
+# Define LUMO Level
+    LUMO = HOMO + 1
 
 with open(filename, 'rb') as datafile:
     # Get data
@@ -44,8 +57,8 @@ E = h*c/maxWavelength
 print("Energy gap: {0} eV".format(E))
 
 # Get Energy Level Transition
-n = np.sqrt((8*E*me*maxWavelength*2)/(h**2))
-print("Energy level: {0}".format(n))
+L = np.sqrt((h**2*HOMO**2)/(8*me*E))
+print("Length of {0}: {1}".format(molecule, L))
 
 
 # Plot wavelength/absorption
