2bb31a07085918049c71fffa526089ba1db5713c
Daniel Underwood <daniel.underwood13@gmail.com>
2014-10-24 08:13:42 -0400

    Add labels to plot and add optional command line parameter for molecule name

--- dataanalysis.py
+++ dataanalysis.py
@@ -2,8 +2,8 @@ import sys
 import csv
 import matplotlib.pyplot as plt
 
-if len(sys.argv) != 2 or sys.argv[1] == 'help' or sys.argv[1] == 'h' or sys.argv[1] == '--help':
-    print "Usage: python dataanalysis.py <filename>"
+if len(sys.argv) < 2 or sys.argv[1] == 'help' or sys.argv[1] == 'h' or sys.argv[1] == '--help':
+    print "Usage: python dataanalysis.py <filename> [molecule]"
     quit()
 
 # Define Constants
@@ -11,8 +11,12 @@ h = 4.135667e-15    # Planck's constant in eV*s
 c = 299792458e9    # Speed of Light in nm/s
 
 
-# Set Data filename
+# Set Data filename and molecule name
 filename = sys.argv[1]
+if len(sys.argv) == 3:
+    molecule = sys.argv[2]
+else:
+    molecule = filename
 
 with open(filename, 'rb') as datafile:
     # Get data
@@ -31,7 +35,7 @@ with open(filename, 'rb') as datafile:
 # Assuming this means wavelength of max absorption
 maxAbsIndex = absorption.index(max(absorption))
 maxWavelength = wavelength[maxAbsIndex]
-print("Maximum wavelength: {0}".format(maxWavelength))
+print("Maximum wavelength: {0} nm".format(maxWavelength))
 
 # Energy Gap
 # E = hc/lambda
@@ -39,7 +43,10 @@ E = h*c/maxWavelength
 print("Energy gap: {0} eV".format(E))
 
 
+# Plot wavelength/absorption
 plt.plot(wavelength, absorption)
+plt.title("Wavelength vs. Absorption for {0}".format(molecule))
+plt.axis([min(wavelength), max(wavelength), min(absorption), max(absorption)])
 plt.show()
 
 
